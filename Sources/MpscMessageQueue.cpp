ed4ee2985d83418b2ce4f88c7fa8a7b84901e3f9
floweryclover <floweryclover@gmail.com>
2025-05-01 18:54:16 +0900

    feat: MpscMessageQueue

--- Sources/MpscMessageQueue.cpp
+++ Sources/MpscMessageQueue.cpp
@@ -4,32 +4,71 @@
 
 #include "MpscMessageQueue.h"
 
-void MpscMessageQueue::Enqueue(uint64_t sessionId,
-                               uint16_t messageType,
-                               uint16_t messageBodySize,
-                               const char* messageBody)
+bool MpscMessageQueue::Push(uint64_t sessionId,
+                            uint16_t messageType,
+                            uint16_t messageBodySize,
+                            const char* messageBody)
 {
+    int poolIndex;
+    size_t allocateSize;
+    if (!TryGetPoolIndexForSize(messageBodySize, poolIndex, allocateSize))
+    {
+        return false;
+    }
+    std::shared_lock lock{ mutex_ };
+    std::lock_guard producerLock{ producerMutex_ };
 
-    std::lock_guard lock{ mutex_ };
+    Message message{};
+    message.SessionId = sessionId;
+    message.MessageType = messageType;
+    message.MessageBodySize = messageBodySize;
 
+    if (messageBodySize > 0)
+    {
+        auto& pool = pools_[currentPushIndex_][poolIndex];
+
+        if (pool.empty())
+        {
+            const auto beforePoolSize = pool.size();
+            for (int i = 0; i < beforePoolSize; ++i)
+            {
+                pool.emplace_back(std::make_unique<char[]>(allocateSize));
+            }
+        }
+
+        auto block = std::move(pool.back());
+        pool.pop_back();
+
+        memcpy_s(block.get(), messageBodySize, messageBody, messageBodySize);
+        message.MessageBody = std::move(block);
+    }
+
+    auto& pushQueue = queues_[currentPushIndex_];
+    pushQueue.emplace(std::move(message));
+
+    return true;
 }
 
 void MpscMessageQueue::Pop()
 {
     auto& popQueue = queues_[1 - currentPushIndex_];
-    auto& pool = pools_[1 - currentPushIndex_];
 
     if (popQueue.empty())
     {
         return;
     }
 
+    int poolIndex;
+    size_t allocateSize;
+    TryGetPoolIndexForSize(popQueue.front().MessageBodySize, poolIndex, allocateSize);
+
+    auto& pool = pools_[1 - currentPushIndex_][poolIndex];
     pool.emplace_back(std::move(popQueue.front().MessageBody));
     popQueue.pop();
 }
 
 void MpscMessageQueue::Swap()
 {
-    std::lock_guard lock{ mutex_ };
+    std::unique_lock lock{ mutex_ };
     currentPushIndex_ = 1 - currentPushIndex_;
 }
--- Sources/MpscMessageQueue.h
+++ Sources/MpscMessageQueue.h
@@ -5,17 +5,41 @@
 #ifndef RATKINIASERVER_MPSCMESSAGEQUEUE_H
 #define RATKINIASERVER_MPSCMESSAGEQUEUE_H
 
+#include "RatkiniaProtocol.h"
 #include <memory>
 #include <queue>
+#include <array>
 #include <shared_mutex>
 
 class MpscMessageQueue final
 {
 public:
-    void Enqueue(uint64_t sessionId,
-                 uint16_t messageType,
-                 uint16_t messageBodySize,
-                 const char* messageBody);
+    bool Push(uint64_t sessionId,
+              uint16_t messageType,
+              uint16_t messageBodySize,
+              const char* messageBody);
+
+    /**
+     * 소비자 스레드에서 호출.
+     * @return
+     */
+     [[nodiscard]]
+    __forceinline bool TryPeek(uint64_t& outSessionId, uint16_t& outMessageType, uint16_t& outMessageBodySize, const char*& outMessageBody) const
+    {
+        auto& popQueue = queues_[1 - currentPushIndex_];
+        if (popQueue.empty())
+        {
+            return false;
+        }
+
+        auto& front = popQueue.front();
+        outSessionId = front.SessionId;
+        outMessageType = front.MessageType;
+        outMessageBodySize = front.MessageBodySize;
+        outMessageBody = front.MessageBody.get();
+
+        return true;
+    }
 
     /**
      * 소비자 스레드에서 호출.
@@ -28,6 +52,15 @@ public:
     void Swap();
 
 private:
+    static constexpr int PoolIndex_1_16 = 0;
+    static constexpr int PoolIndex_17_32 = 0;
+    static constexpr int PoolIndex_33_64 = 1;
+    static constexpr int PoolIndex_65_128 = 2;
+    static constexpr int PoolIndex_129_256 = 3;
+    static constexpr int PoolIndex_257_512 = 4;
+    static constexpr int PoolIndex_513_1024 = 5;
+    static constexpr int PoolIndex_1025_MaxMessageBodySize = 6;
+
     struct Message
     {
         uint64_t SessionId;
@@ -37,9 +70,62 @@ private:
     };
     int currentPushIndex_;
 
-    std::vector<std::unique_ptr<char[]>> pools_[2];
+    std::array<std::array<std::vector<std::unique_ptr<char[]>>, 7>, 2> pools_;
     std::queue<Message> queues_[2];
-    std::mutex mutex_;
+    std::shared_mutex mutex_;
+    std::mutex producerMutex_;
+
+    __forceinline static bool TryGetPoolIndexForSize(const size_t inSize, int& outIndex, size_t& outAllocateSize)
+    {
+        if (inSize > RatkiniaProtocol::MessageMaxSize - RatkiniaProtocol::MessageHeaderSize)
+        {
+            return false;
+        }
+
+        if (inSize > 1024)
+        {
+            outIndex = PoolIndex_1025_MaxMessageBodySize;
+            outAllocateSize = RatkiniaProtocol::MessageMaxSize - RatkiniaProtocol::MessageHeaderSize;
+        }
+        else if (inSize > 512)
+        {
+            outIndex = PoolIndex_513_1024;
+            outAllocateSize = 1024;
+        }
+        else if (inSize > 256)
+        {
+            outIndex = PoolIndex_257_512;
+            outAllocateSize = 512;
+        }
+        else if (inSize > 128)
+        {
+            outIndex = PoolIndex_129_256;
+            outAllocateSize = 256;
+        }
+        else if (inSize >  64)
+        {
+            outIndex = PoolIndex_65_128;
+            outAllocateSize = 128;
+        }
+        else if (inSize > 32)
+        {
+            outIndex = PoolIndex_33_64;
+            outAllocateSize = 1024;
+            outAllocateSize = 64;
+        }
+        else if (inSize > 16)
+        {
+            outIndex = PoolIndex_17_32;
+            outAllocateSize = 32;
+        }
+        else
+        {
+            outIndex = PoolIndex_1_16;
+            outAllocateSize = 16;
+        }
+
+        return true;
+    }
 };
 
 #endif //RATKINIASERVER_MPSCMESSAGEQUEUE_H
