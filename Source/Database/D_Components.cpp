339c5958b783ee4efe6ae664f7e596cf3fe10298
floweryclover <floweryclover@gmail.com>
2025-09-30 01:27:56 +0900

    wip

---
+++ Source/Database/D_Components.cpp
@@ -0,0 +1,19 @@
+//
+// Created by floweryclover on 2025-09-30.
+//
+
+#include "D_Components.h"
+#include "ComponentManager.h"
+
+D_Components::D_Components(pqxx::connection& connection)
+    : Database{connection}
+{
+
+}
+
+void D_Components::Flush(ComponentManager& componentManager)
+{
+    for (auto& sparseSet : componentManager.SparseSets())
+    {
+    }
+}
---
+++ Source/GlobalObject/G_PersistenceTimer.h
@@ -0,0 +1,15 @@
+//
+// Created by floweryclover on 2025-09-30.
+//
+
+#ifndef G_PERSISTENCETIMER_H
+#define G_PERSISTENCETIMER_H
+
+#include "GlobalObject.h"
+
+struct G_PersistenceTimer final
+{
+    GLOBALOBJECT(G_PersistenceTimer)
+};
+
+#endif //G_PERSISTENCETIMER_H
